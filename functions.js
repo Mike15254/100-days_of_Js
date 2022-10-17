7eccd7a7c38b606e29369d43d10e1e5f243a5d8b
Mike15254 <mike8max10@gmail.com>
2022-10-17 21:39:13 +0300

    first js script

---
+++ functions.js
@@ -0,0 +1,4 @@
+// A function that converts a value to a number
+let theNumber = Number(prompt("Pick a number"));
+console.log("Your number is the square root of " +
+            theNumber * theNumber);
