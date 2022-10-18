fdaf335b3103dd902463d238e2db61c9b6fc29f8
Mike15254 <mike8max10@gmail.com>
2022-10-18 21:54:00 +0300

    loop

---
+++ loops.js
@@ -0,0 +1,8 @@
+// example of a if loop in js
+let iceCream = "vanilla";
+
+if (iceCream === "vanila") {
+    console.log("Yeah, I love vanila ice cream!");
+} else {
+    console.log("No, I prefer chocolate.");
+}
