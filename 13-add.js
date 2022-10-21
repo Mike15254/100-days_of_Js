15f2d4d82c83f533927e2598fb933e121995bcfd
Mike15254 <mike8max10@gmail.com>
2022-10-21 18:00:06 +0300

    first commit

---
+++ 13-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+// A script to add two numbers together using a function
+// The function must be visible from outside
+// The name of the function must be add .
+// You are not allowed to use
+
+
+exports.add = function (a , b) {
+    return a + b;
+}
