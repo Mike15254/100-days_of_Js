30cdb64ac6526125441c91d05aaf1c30f922312e
Mike15254 <mike8max10@gmail.com>
2022-10-22 19:15:27 +0300

    first commit

---
+++ Part_two/0-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// A script that writes an empty class Rectangle that defines a rectangle
+// You must use class notation to define your class
+
+class Rectangle {
+
+} 
+module.exports = Rectangle;
---
+++ Part_two/README.md
@@ -0,0 +1,2 @@
+# This Know the second part of learning Javascript
+Understanding more in Javascript 
