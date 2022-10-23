e2ac864482020f33eff826438b21cffcd4299f1c
Mike15254 <mike8max10@gmail.com>
2022-10-23 21:16:33 +0300

    first commit

---
+++ Part_two/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Write a class rectangle that defines a rectangle
+  //  You must use class notation for definig your class
+  // The constructor must take 2 argumnets w and h
+  // Initialize the instances attribute width with the value of w and height with the value of h
+
+class Rectangle {
+    constructor (w, h) {
+        this.width = w;
+        this.height = h;
+    }
+}
+module.exports = Rectangle;
