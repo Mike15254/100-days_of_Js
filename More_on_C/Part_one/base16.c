b2f2798e2a13f21d2afa1a6075e86c5dba1cc0b1
Mike15254 <mike8max10@gmail.com>
2022-11-05 17:59:47 +0300

    first commit

---
+++ More_on_C/Part_one/base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ * Return - Always (0) success 
+*/
+int main(void)
+{
+int i;
+
+    for (i = 48 ; i <58; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+    return (0);
+}
