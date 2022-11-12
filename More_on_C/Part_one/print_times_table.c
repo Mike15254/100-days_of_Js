4983be163327dcea12c3f66a679354f9f4372ee1
Mike15254 <mike8max10@gmail.com>
2022-11-12 21:43:43 +0300

    first commit

---
+++ More_on_C/Part_one/print_times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * if n is greater than 15 or less than 0 the function should
+ * print anything
+ * 
+ * @n: input integer 
+ * 
+ */
