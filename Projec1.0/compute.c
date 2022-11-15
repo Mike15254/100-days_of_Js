de664db33b1b099cf4a5bf3110891d2e1c3fe3ed
Mike15254 <mike8max10@gmail.com>
2022-11-15 16:03:07 +0300

    first commit

---
+++ Projec1.0/compute.c
@@ -0,0 +1,12 @@
+#include "project.h"
+
+/**
+ * compute_expense : Finds the total expense for each item
+ * for the four months and the budget deficite
+ * 
+ * Return: Always 0
+ */
+int compute_expence(int n)
+{
+
+}
