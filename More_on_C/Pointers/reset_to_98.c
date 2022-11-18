422cd32d10df1f57a55677a4b2447910c5540df5
Mike15254 <mike8max10@gmail.com>
2022-11-18 12:45:34 +0300

    initial commit

---
+++ More_on_C/Pointers/reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * void reset_to_98 - updates the value it points to to 98
+ *
+ * @c: pointer to an integer
+ *
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
