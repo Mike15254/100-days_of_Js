7f0bb6d09b86eabc60da7eaacd40046e4830728e
Mike15254 <mike8max10@gmail.com>
2022-11-19 18:52:24 +0300

    first commit

---
+++ More_on_C/Pointers/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - It shows the length of a string
+ * 
+ * @s: The string
+ * 
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+    int i=0;
+
+    while (*(s + i) != '\0')
+    {
+        i++;
+    }
+    return (i);
+
+}
